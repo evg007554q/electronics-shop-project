2882eeca0d6ca5ccf81749dd390ffbba759aedab
evg007 <evg007554@yandex.ru>
2023-05-22 22:39:07 +0300

    item+test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,27 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item1():
+    return Item("Смартфон", 10000, 20)
+
+@pytest.fixture
+def item2():
+    return Item("Ноутбук", 20000, 5)
+
+
+def test_calculate_total_price(item1):
+    assert item1.calculate_total_price() == 200000
+
+def test_calculate_total_price1(item2):
+    assert item2.calculate_total_price() == 100000
+
+#Item.pay_rate = 0.8
+
+def test_apply_discount(item1):
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.calculate_total_price() == 160000
+
